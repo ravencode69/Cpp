d53c39d6bbb17ee8679fd76c0bbfef84a73799e9
Raven <1906569@kiit.ac.in>
2021-07-07 17:49:17 +0530

    Secondcommit

--- Trees/Queue.h
+++ Trees/Queue.h
@@ -8,7 +8,7 @@ struct Node
 
 struct Queue
 {
-    Node **arr; // Node ** isliye banaya kyuki "node type ke pointer" ko sotre karna h na ek array mein isliye
+    struct Node **arr; // Node ** isliye banaya kyuki "node type ke pointer" ko sotre karna h na ek array mein isliye
     int size;
     int front;
     int rear;
@@ -17,9 +17,9 @@ void create(struct Queue *q, int s)
 {
     q->front = q->rear = 0;
     q->size = s;
-    q->arr = (Node **)malloc(sizeof(Node *) * q->size);
+    q->arr = (struct Node **)malloc(sizeof(struct Node *) * q->size);
 }
-void Enqueue(struct Queue *q, Node *x)
+void Enqueue(struct Queue *q, struct Node *x)
 {
     if ((q->rear + 1) % q->size == q->front)
         printf("Q is full");
@@ -29,9 +29,9 @@ void Enqueue(struct Queue *q, Node *x)
         q->arr[q->rear] = x;
     }
 }
-Node *deQ(struct Queue *q)
+struct Node *deQ(struct Queue *q)
 {
-    Node *x = NULL;
+    struct Node *x = NULL;
     if (q->front == q->rear)
         printf("q is empty");
     else
