085fd15f6eedbc8eeb109dd37f23389e0bb9239d
Raven <1906569@kiit.ac.in>
2021-07-18 14:18:11 +0530

    merge sort

--- CN-Dsa/Recursion2/removeDuplicates.cpp
+++ CN-Dsa/Recursion2/removeDuplicates.cpp
@@ -4,23 +4,24 @@ using namespace std;
 
 void remove(char str[])
 {
-    if (strlen(str) == 0)
+    if (strlen(str) == 1)
         return;
+
     remove(str + 1);
     if (str[0] == str[1])
     {
-        int i;
-        for (i = 1; i != '\0'; i++)
+
+        for (int i = 1; i <= strlen(str); i++)
         {
             str[i - 1] = str[i];
         }
-        str[i] = '\0';
     }
 }
 int main()
 {
     char str[30];
     cin >> str;
+
     remove(str);
     cout << str;
 }
