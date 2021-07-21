0857d200d2cea025b6816f781cc3a93547796866
Raven <1906569@kiit.ac.in>
2021-07-21 07:57:04 +0530

    Merge sort

--- CN-Dsa/Recursion2/MergeSort.cpp
+++ CN-Dsa/Recursion2/MergeSort.cpp
@@ -1,17 +1,53 @@
 #include <iostream>
 using namespace std;
-void MergesortN(int arr[], int s, int mid, int l)
+void finalarray(int input[], int si, int ei, int mid)
 {
+    int i = si;
+    int j = mid + 1;
+    int f[500];
+    int k = si;
+    while (i <= mid && j <= ei)
+    {
+        if (input[i] < input[j])
+        {
+            f[k++] = input[i++];
+        }
+        else
+        {
+            f[k++] = input[j++];
+        }
+    }
+    while (i <= mid)
+    {
+        f[k++] = input[i++];
+    }
+    while (j <= ei)
+    {
+        f[k++] = input[j++];
+    }
+    for (i = si; i <= ei; i++)
+    {
+        input[i] = f[i];
+    }
 }
 
-void Mergesort(int arr[], int s, int l)
+void helper(int input[], int si, int ei)
 {
-    if (l <= s)
+    if (si >= ei)
+    {
         return;
-    int mid = s + l / 2;
-    Mergesort(arr, s, mid);
-    Mergesort(arr, mid + 1, l);
-    MergesortN(arr, s, mid, l);
+    }
+    int mid = (si + ei) / 2;
+    helper(input, si, mid);
+    helper(input, mid + 1, si);
+    finalarray(input, si, ei, mid);
+}
+
+void mergeSort(int input[], int size)
+{
+    int ei = size - 1;
+
+    helper(input, 0, ei);
 }
 
 int main()
@@ -24,10 +60,10 @@ int main()
     {
         cin >> arr[i];
     }
-    int l = size - 1;
-    Mergesort(arr, 0, l);
+    int l = size;
+    mergeSort(arr, l);
     for (int i = 0; i < size; i++)
     {
-        cout << arr[i];
+        cout << arr[i] << " ";
     }
 }
