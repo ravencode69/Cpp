5cafaf03a75b524e5135dfef72b06ae9582da518
Raven <1906569@kiit.ac.in>
2021-07-25 12:48:27 +0530

    Mergwe sort

--- CN-Dsa/Recursion2/MergeSort.cpp
+++ CN-Dsa/Recursion2/MergeSort.cpp
@@ -1,69 +1,67 @@
 #include <iostream>
 using namespace std;
-void finalarray(int input[], int si, int ei, int mid)
+void merging(int input[], int low, int mid, int high)
 {
-    int i = si;
-    int j = mid + 1;
-    int f[500];
-    int k = si;
-    while (i <= mid && j <= ei)
-    {
-        if (input[i] < input[j])
-        {
-            f[k++] = input[i++];
-        }
-        else
-        {
-            f[k++] = input[j++];
-        }
-    }
-    while (i <= mid)
-    {
-        f[k++] = input[i++];
-    }
-    while (j <= ei)
-    {
-        f[k++] = input[j++];
-    }
-    for (i = si; i <= ei; i++)
-    {
-        input[i] = f[i];
-    }
-}
 
-void helper(int input[], int si, int ei)
+    int b[200];
+    int l1 = low, l2 = mid + 1, i = 0;
+    while (l1 <= mid && l2 <= high)
+    {
+        if (input[l1] <= input[l2])
+            b[i++] = input[l1++];
+        else
+            b[i++] = input[l2++];
+    } // end while loop here
+    while (l1 <= mid)
+    {
+        b[i++] = input[l1++];
+    }
+
+    while (l2 <= high)
+    {
+        b[i++] = input[l2++];
+    }
+
+    int j = 0;
+    for (i = low; i <= high; i++)
+        input[i] = b[j++];
+
+    //   }   dont end it here
+}
+void mergeSort1(int input[], int first, int last)
 {
-    if (si >= ei)
+
+    if (first < last)
+    {
+        int mid = (first + last) / 2;
+        mergeSort1(input, first, mid);
+        mergeSort1(input, mid + 1, last);
+        merging(input, first, mid, last);
+    }
+    else
     {
         return;
     }
-    int mid = (si + ei) / 2;
-    helper(input, si, mid);
-    helper(input, mid + 1, si);
-    finalarray(input, si, ei, mid);
 }
-
-void mergeSort(int input[], int size)
+void mergeSort(int input[], int length)
 {
-    int ei = size - 1;
-
-    helper(input, 0, ei);
+    if (length <= 0)
+    {
+        return;
+    }
+    mergeSort1(input, 0, length - 1);
 }
 
 int main()
 {
-    int arr[30];
-    int size;
-    cout << " Input size ";
-    cin >> size;
-    for (int i = 0; i < size; i++)
+    int length;
+    cin >> length;
+    int *input = new int[length];
+    for (int i = 0; i < length; i++)
+        cin >> input[i];
+    mergeSort(input, length);
+    for (int i = 0; i < length; i++)
     {
-        cin >> arr[i];
-    }
-    int l = size;
-    mergeSort(arr, l);
-    for (int i = 0; i < size; i++)
-    {
-        cout << arr[i] << " ";
+        cout << input[i] << " ";
     }
 }
