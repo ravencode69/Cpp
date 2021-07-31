91a2bf807562fd94f471a61ba3b9abcd3837b320
Raven <1906569@kiit.ac.in>
2021-07-31 23:39:04 +0530

    st

--- CN-Dsa/Recursion2/ReturnsumtoK.cpp
+++ CN-Dsa/Recursion2/ReturnsumtoK.cpp
@@ -64,3 +64,53 @@ int main()
         cout << endl;
     }
 }
+/*
+int subsetSumToK(int input[], int n, int output[][50], int k) {
+    
+    if(n==0)
+    {
+        if(k==0)
+        {
+            output[0][0]=0;                  
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+    
+    int op1[1000][50];
+    int op2[1000][50];
+    
+ 
+    int s2=subsetSumToK(input+1,n-1,op2,k);
+    int s1=subsetSumToK(input+1,n-1,op1,k-input[0]);
+    
+    int rownumber=0;
+    
+ 
+    for(int i=0;i<s2;i++)
+    {
+        for(int j=0;j<=op2[i][0];j++)
+        {
+            output[rownumber][j]=op2[i][j];
+        }
+        rownumber++;
+    }
+    
+
+    for(int i=0;i<s1;i++)
+    { 
+      output[rownumber][0]=op1[i][0]+1;
+      output[rownumber][1]=input[0];
+        for(int j=1;j<=op1[i][0];j++)
+        {
+            output[rownumber][j+1]=op2[i][j];
+        }
+        rownumber++;
+    }    
+   
+return rownumber;
+}
+
+*/
