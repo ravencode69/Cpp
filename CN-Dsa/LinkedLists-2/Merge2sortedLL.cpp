7d99fd2a8e939cd09f622548dafdcab13712b9db
Raven <1906569@kiit.ac.in>
2021-08-24 16:22:02 +0530

    Linked lists

---
+++ CN-Dsa/LinkedLists-2/Merge2sortedLL.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+using namespace std;
+
+Node *takeinput()
+{
+    int data;
+    cin >> data;
+    Node *head = NULL, *tail = NULL;
+    while (data != -1)
+    {
+        Node *newNode = new Node(data);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+Node *mergeTwoSortedLinkedLists(Node *head1, Node *head2)
+{
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        Node *head1 = takeinput();
+        Node *head2 = takeinput();
+        Node *head3 = mergeTwoSortedLinkedLists(head1, head2);
+        print(head3);
+    }
+    return 0;
+}
