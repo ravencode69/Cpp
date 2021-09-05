9a5f1e22ded834d33c2605ad9341c8032a89b944
Raven <1906569@kiit.ac.in>
2021-09-06 02:54:19 +0530

    Tree Class Creation

---
+++ CN-Dsa/Trees/2_treeClass.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class Treenode
+{
+public:
+    T data;
+    vector<Treenode<T> *> children;
+    Treenode(T data)
+    {
+        this->data = data;
+    }
+};
+int main()
+{
+    Treenode<int> *root = new Treenode(1);
+    Treenode<int> *node1 = new Treenode(2);
+    Treenode<int> *node2 = new Treenode(3);
+    root->children.push_back(node1);
+    root->children.push_back(node2);
+}
