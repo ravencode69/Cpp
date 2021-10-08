a21bc8b40711758479b7894bbb264837e328bf9e
Raven <1906569@kiit.ac.in>
2021-10-08 22:30:42 +0530

    Intro to HashMap

---
+++ CN-Dsa/HashMaps/1_introtoHM.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_map> //hashtable wala O(1)
+
+using namespace std;
+
+int main()
+{
+    unordered_map<int, int> map; // its just a key value pair
+
+    //How to Insert ?
+    pair<int, int> p(2, 3);
+    map.insert(p);
+
+    /*--------------OR---------------*/
+    map[5] = 6;
+    //How to find or acess
+    cout << map[2];
+    cout << map.at(2);
+
+    /*check presense of an element*/
+    if (map.count(2))
+    {
+        cout << "\nElement Found" << endl;
+    }
+    /* Map size*/
+    cout << map.size();
+    /*Erase from map*/
+    map.erase(2);
+    cout << map.size();
+
+    return 0;
+}
