a2ddcfd7e91adc44b80dd9b590446acb974a5bfb
Raven <1906569@kiit.ac.in>
2022-01-05 20:07:31 +0530

    All possible ways

---
+++ CN-Dsa/15_DP-2/7_AllPW.cpp
@@ -0,0 +1,12 @@
+/*All possible ways
+Send Feedback
+Given two integers a and b. You need to find and return the count of possible ways
+in which we can represent the number a as the sum of unique integers raise to the power b.
+For example: if a = 10 and b = 2, only way to represent 10 as sum of unique integers raised to power 2 is-*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int getAllWays(int a, int b)
+{
+    // Write your code here
+}
