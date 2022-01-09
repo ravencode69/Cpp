edffef823f2e8ff1a683b45996c7661696a432e4
Raven <1906569@kiit.ac.in>
2022-01-10 01:15:53 +0530

    Stack Template

--- CN-Dsa/5_StackAndQ/StackByTemplate.cpp
+++ CN-Dsa/5_StackAndQ/StackByTemplate.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-/*template help us to create a stack of any data type*/ template <typename T> //Template defination
+/*template help us to create a stack of any data type*/ template <typename T> // Template defination
 
 class stack
 {
@@ -15,13 +15,13 @@ public:
     {
         array = new T[capacity];
         ci = 0;
-        capacity = 3; //took this value any number that i wanted.
+        capacity = 3; // took this value any number that i wanted.
     }
 
-    void push(int data)     //the concept is really simple just when the ci becomes equal to capacity we'll
-    {                       //make a new array of double its size and copy the elements from prev  arr then
+    void push(int data)     // the concept is really simple just when the ci becomes equal to capacity we'll
+    {                       // make a new array of double its size and copy the elements from prev  arr then
         if (ci == capacity) // we'll delete the prev array and point the array to the new array and henceforth we'll
-        {                   //multiply the capacity by 2
+        {                   // multiply the capacity by 2
             T *narray = new T[capacity * 2];
             for (int i = 0; i < capacity; i++)
             {
