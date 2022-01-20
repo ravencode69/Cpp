b6bfd246784b1488b20d9f911c28977a3fe4fb76
Raven <1906569@kiit.ac.in>
2022-01-20 18:17:13 +0530

    Maximal sq

---
+++ CN-Dsa/15_DP-2/10_MaximalSquare.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMaxSquareWithAllZeros(int **arr, int row, int col)
+{
+
+    int dp[row][col] = {0};
+
+    int ans = 0;
+    for (int i = 0; i < row; i++)
+    {
+        dp[i][0] = (arr[i][0]) ? 0 : 1;
+        ans = max(ans, dp[i][0]);
+    }
+    for (int i = 0; i < col; i++)
+    {
+        dp[0][i] = (arr[0][i]) ? 0 : 1;
+        ans = max(ans, dp[0][i]);
+    }
+
+    for (int i = 1; i < row; i++)
+    {
+        for (int j = 1; j < col; j++)
+        {
+
+            if (arr[i][j] == 1)
+                dp[i][j] = 0;
+            else
+                dp[i][j] = 1 + min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1]));
+            ans = max(ans, dp[i][j]);
+        }
+    }
+    return ans;
+}
