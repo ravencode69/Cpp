561a75ae804928f3ed9ccfbcbaddc414ac16b179
Raven <1906569@kiit.ac.in>
2022-01-24 21:52:52 +0530

    maxof3

--- CN-Dsa/Arrays/7_maxof3.cpp
+++ CN-Dsa/Arrays/7_maxof3.cpp
@@ -14,7 +14,7 @@ public:
         int min1 = (int)1e9;
         int min2 = (int)1e9;
 
-        for (int i = 0; i <= nums.size(); i++)
+        for (int i = 0; i < nums.size(); i++)
         {
 
             if (nums[i] > max1)
@@ -45,49 +45,5 @@ public:
         }
 
         return max(max1 * min1 * min2, max1 * max2 * max3);
-    }
-};
-class Solution
-{
-public:
-    int maximumProduct(vector<int> &nums)
-    {
-        int mini = (int)1e9;
-        int smin = (int)1e9;
-
-        int maxi = -(int)1e9;
-        int smax = maxi;
-        int tmax = maxi;
-
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (nums[i] > maxi)
-            {
-                tmax = smax;
-                smax = maxi;
-                maxi = nums[i];
-            }
-            else if (nums[i] > smax)
-            {
-                tmax = smax;
-                smax = nums[i];
-            }
-            else if (nums[i] > tmax)
-            {
-                tmax = nums[i];
-            }
-
-            if (nums[i] < mini)
-            {
-                smin = mini;
-                mini = nums[i];
-            }
-            else if (nums[i] < smin)
-            {
-                smin = nums[i];
-            }
-        }
-
-        return max(maxi * smax * tmax, maxi * mini * smin);
     }
 };
