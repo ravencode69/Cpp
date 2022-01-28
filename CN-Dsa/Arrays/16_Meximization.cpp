f0e69e1e369fdb71172f4914dff783366be4caca
Raven <1906569@kiit.ac.in>
2022-01-28 23:59:57 +0530

    m

--- CN-Dsa/Arrays/16_Meximization.cpp
+++ CN-Dsa/Arrays/16_Meximization.cpp
@@ -1,40 +1,82 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// #define ll long long
+
+// int main()
+// {
+//     int t;
+//     cin >> t;
+
+//     while (t--)
+//     {
+//         int n;
+//         cin >> n;
+//         vector<int> ans(n);
+//         vector<int> out, out2;
+//         map<int, int> map;
+//         for (int i = 0; i < n; i++)
+//         {
+//             cin >> ans[i];
+
+//             if (map[ans[i]] == 1)
+//             {
+//                 out2.push_back(ans[i]);
+//             }
+//             else
+//             {
+//                 out.push_back(ans[i]);
+//                 map[ans[i]]++;
+//             }
+//         }
+
+//         sort(out.begin(), out.end());
+//         for (int i = 0; i < out.size(); i++)
+//             cout << out[i] << " ";
+//         for (int i = 0; i < out2.size(); i++)
+//             cout << out2[i] << " ";
+//         cout << endl;
+//     }
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
-
-#define ll long long
 
 int main()
 {
     int t;
     cin >> t;
-
     while (t--)
     {
         int n;
         cin >> n;
-        vector<int> ans(n);
-        vector<int> out, out2;
+        vector<int> v, v2;
+        vector<int> inp(n);
         map<int, int> map;
+
         for (int i = 0; i < n; i++)
         {
-            cin >> ans[i];
-
-            if (map[ans[i]] == 1)
+            cin >> inp[i];
+            if (map[inp[i]] == 1)
             {
-                out2.push_back(ans[i]);
+                v2.push_back(inp[i]);
             }
             else
             {
-                out.push_back(ans[i]);
-                map[ans[i]]++;
+                v.push_back(inp[i]);
+                map[inp[i]]++;
             }
         }
-
-        sort(out.begin(), out.end());
-        for (int i = 0; i < out.size(); i++)
-            cout << out[i] << " ";
-        for (int i = 0; i < out2.size(); i++)
-            cout << out2[i] << " ";
+        sort(v.begin(), v.end());
+        for (int x : v)
+        {
+            cout << x << " ";
+        }
+        for (int x : v2)
+        {
+            cout << x << " ";
+        }
         cout << endl;
     }
+    return 0;
 }
