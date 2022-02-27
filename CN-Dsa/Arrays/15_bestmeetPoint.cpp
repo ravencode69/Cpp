b60fdaf7d7ad3b1becd1d6af8c819fb01da4643e
Raven <1906569@kiit.ac.in>
2022-02-27 11:21:41 +0530

    codefprces

--- CN-Dsa/Arrays/15_bestmeetPoint.cpp
+++ CN-Dsa/Arrays/15_bestmeetPoint.cpp
@@ -47,3 +47,55 @@ public:
         return dist;
     }
 };
+
+/*
+class Solution {
+public:
+
+int minTotalDistance(vector<vector<int>> &grid)
+{
+    // Write your code
+
+    // row wise x collection
+    vector<int> xp;
+    vector<int> yp;
+
+    for (int i = 0; i < grid.size(); i++)
+    {
+
+        for (int j = 0; j < grid[0].size; j++)
+        {
+            if (grid[i][j] == 1)
+                xp.push_back(i);
+        }
+    }
+    // col wise y collection
+    for (int i = 0; i < grid[0].size(); i++)
+    {
+
+        for (int j = 0; j < grid.size(); j++)
+        {
+            if (grid[i][j] == 1)
+                yp.push_back(i);
+        }
+    }
+
+    int x, int y;
+    x = xp[xp.size() / 2];
+    y = yp[yp.size() / 2];
+
+    // dist calc.
+    int dist = 0;
+
+    for (int i : xp)
+        dist += abs(x - i);
+
+    for (int i : yp)
+        dist += abs(y - i);
+
+    return dist;
+}
+}
+;
+
+*/
