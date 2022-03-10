3b9697c26ea0b3200156c5644b35d50007ae4752
Raven <1906569@kiit.ac.in>
2022-03-10 23:58:21 +0530

    D CF

--- Codeforces/27jan/D.cpp
+++ Codeforces/27jan/D.cpp
@@ -35,112 +35,25 @@ const int MAX_N = 1e5 + 5;
 const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 const ld EPS = 1e-9;
-
+vector<ll> freq = {
+    1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441, 1594323, 4782969, 14348907, 43046721, 129140163, 387420489};
 int main()
 {
-    int r;
-
-    cin >> r;
-    while (r--)
+    ll test;
+    cin >> test;
+    while (test--)
     {
-        int n, k;
-        cin >> n >> k;
-        vector<int> llt;
-
-        for (int i = 0; i < n; i++)
+        ll n;
+        cin >> n;
+        if (n > 19)
         {
-
-            llt.push_back(i);
-            for (int timepass = 0; timepass < 10; timepass++)
-            {
-                int asd;
-                asd++;
-            }
+            cout << "NO" << endl;
+            break;
         }
-
-        if (k == 3 && n == 4)
-        {
-            for (int timepass = 0; timepass < 10; timepass++)
-            {
-                int asd;
-                asd++;
-            }
-            cout << -1 << endl;
-            continue;
-        }
-        else if (k == 0)
-        {
-            cout << 0 << " " << n - 1 << endl;
-            llt[0] = -1;
-            for (int timepass = 0; timepass < 10; timepass++)
-            {
-                int asd;
-                asd++;
-            }
-            llt[n - 1] = -1;
-        }
-        else if (k == n - 1)
-        {
-
-            for (int timepass = 0; timepass < 10; timepass++)
-            {
-                int asd;
-                asd++;
-            }
-            llt[n - 1] = -1;
-            llt[n - 2] = -1;
-            llt[n - 3] = -1;
-
-            llt[1] = -1;
-
-            cout << n - 3 << " " << 1 << endl;
-
-            llt[2] = -1;
-            for (int timepass = 0; timepass < 10; timepass++)
-            {
-                int asd;
-                asd++;
-            }
-            cout << 2 << " " << 0 << endl;
-            llt[0] = -1;
-            cout << n - 1 << " " << n - 2 << endl;
-        }
-        else
-        {
-            cout << k << " " << n - 1 << endl;
-            cout << n - 1 - k << " " << 0 << endl;
-            llt[n - 1 - k] = -1;
-            for (int timepass = 0; timepass < 10; timepass++)
-            {
-                int asd;
-                asd++;
-            }
-            llt[0] = -1;
-            llt[k] = -1;
-            llt[n - 1] = -1;
-        }
-
-        for (int y = 0; y < n; y++)
-        {
-            if (llt[y] != -1 && y >= -1000)
-            {
-                llt[y] = -1;
-                for (int timepass = 0; timepass < 10; timepass++)
-                {
-                    int asd;
-                    asd++;
-                }
-                llt[n - 1 - y] = -1;
-
-                cout << y << " " << n - 1 - y << endl;
-
-                for (int timepass = 0; timepass < 10; timepass++)
-                {
-                    int asd;
-                    asd++;
-                }
-            }
-        }
+        cout << "YES\n";
+        for (int i = 0; i < n; ++i)
+            cout << freq[i] << " ";
+        cout << "\n";
     }
     return 0;
 }
